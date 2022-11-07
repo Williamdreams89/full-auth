63cc9194bb7bd3166d613f50f60fd53676e07b26
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2022-11-07 19:12:43 +0000

    Initializing accountsapp confs

--- fullauthapi/settings.py
+++ fullauthapi/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accountsapp"
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Configure customed user model
+
+AUTH_USER_MODEL = "accountsapp.User"
