904192fcadc9813e6ee1dab672caaf7c17f33182
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2022-11-07 20:02:59 +0000

    Registering users to django administration

--- accountsapp/admin.py
+++ accountsapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "first_name", "last_name", "department", "date_added"]
+admin.site.register(User, UserAdmin)
