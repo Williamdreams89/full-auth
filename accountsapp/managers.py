4c8b47aba359038e65f19879849d93ca9278b8fb
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2022-11-07 19:38:11 +0000

    Customed User Manager config

--- accountsapp/managers.py
+++ accountsapp/managers.py
@@ -1,4 +1,22 @@
 from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
-    pass
+    """Overiding default user-creation utilities since login have been changed"""
+    def create_user(self, email, first_name, last_name, department, password=None):
+        """Create normal user utility"""
+        if not email:
+            raise ValueError("Email Must not be empty!!!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, department=department)
+        user.set_password(password) #hashing password
+        user.save(using=self._db)
+        return user 
+
+
+    def create_superuser(self, email, first_name, last_name, department, password):
+        """Create Superusers utility"""
+        user = self.create_user(email, first_name, last_name, department, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user 
